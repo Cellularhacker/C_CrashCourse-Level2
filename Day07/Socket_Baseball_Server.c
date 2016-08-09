260cd0d84720f66b55505374b1076dc6cc0d0b8a
Cellularhacker <Cellularhacker@users.noreply.github.com>
2016-08-09 16:31:01 +0900

    Fixed reference method in structs.
    changed some '.' to '->' because of 'call by refernece'

--- Day07/Socket_Baseball_Server.c
+++ Day07/Socket_Baseball_Server.c
@@ -91,7 +91,7 @@ int main(int argc, char *argv[])
 			
 			CheckBBR(&bb_srv, &bb_cli);
 			
-			send(hClntSock, (const char *)&bbr_cli, sizeof(BBR), 0);
+			send(hClntSock, (char *)&bbr_cli, sizeof(BBR), 0);
 		}
 		
 		
@@ -111,47 +111,47 @@ int main(int argc, char *argv[])
 
 
 void resetBBR(BBR *baseball_result_struct) {
-	baseball_result_struct.ball = 0;
-	baseball_result_struct.strike = 0;
-	baseball_result_struct.key++;
+	baseball_result_struct->ball = 0;
+	baseball_result_struct->strike = 0;
+	baseball_result_struct->key++;
 }
 
 void initBBR(BBR *baseball_result_struct) {
-	baseball_result_struct.ball = 0;
-	baseball_result_struct.strike = 0;
-	baseball_result_struct.key = 0;
+	baseball_result_struct->ball = 0;
+	baseball_result_struct->strike = 0;
+	baseball_result_struct->key = 0;
 }
 
 
 void initBB(BB *baseball_struct) {
 	srand((unsigned)time(NULL));
 	
-	baseball_struct.arr[0] = (rand() %10);
-	while(baseball_struct.arr[1] != baseball_struct.arr[0]) baseball_struct.arr[1] = (rand() %10);
-	while( (baseball_struct.arr[2] != baseball_struct.arr[0]) && (baseball_struct.arr[2] != baseball_struct.arr[1])) baseball_struct.arr[2] = (rand() %10);
-	baseball_struct.key = 0;
+	baseball_struct->arr[0] = (rand() %10);
+	while(baseball_struct->arr[1] != baseball_struct->arr[0]) baseball_struct->arr[1] = (rand() %10);
+	while( (baseball_struct->arr[2] != baseball_struct->arr[0]) && (baseball_struct->arr[2] != baseball_struct->arr[1])) baseball_struct->arr[2] = (rand() %10);
+	baseball_struct->key = 0;
 }
 
 void resetBB(BB *baseball_struct) {
 	srand((unsigned)time(NULL));
 	
-	baseball_struct.arr[0] = (rand() %10);
-	while(baseball_struct.arr[1] != baseball_struct.arr[0]) baseball_struct.arr[1] = (rand() %10);
-	while( (baseball_struct.arr[2] != baseball_struct.arr[0]) && (baseball_struct.arr[2] != baseball_struct.arr[1])) baseball_struct.arr[2] = (rand() %10);
-	baseball_struct.key++;
+	baseball_struct->arr[0] = (rand() %10);
+	while(baseball_struct->arr[1] != baseball_struct->arr[0]) baseball_struct->arr[1] = (rand() %10);
+	while( (baseball_struct->arr[2] != baseball_struct->arr[0]) && (baseball_struct->arr[2] != baseball_struct->arr[1])) baseball_struct->arr[2] = (rand() %10);
+	baseball_struct->key++;
 }
 
-BBR CheckBBR(BB *bb_srv, *BB bb_cli) {
+BBR CheckBBR(BB *bb_srv, BB *bb_cli) {
 	BBR bbr_calc;
-	initBBR(bbr_calc);
+	initBBR(&bbr_calc);
 	int for_srv, for_cli;
 	
-	if(bb_srv.key == bb_cli.key) {
-		bbr_calc.key = bb_cli.key;
+	if(bb_srv->key == bb_cli->key) {
+		bbr_calc.key = bb_cli->key;
 		
 		for(for_srv=0;for_srv<3;for_srv++) {
 			for(for_cli=0;for_cli<3;for_cli++) {
-				if(bb_srv.arr[for_srv] == bb_cli.arr[for_cli]) {
+				if(bb_srv->arr[for_srv] == bb_cli->arr[for_cli]) {
 					if(for_srv == for_cli) bbr_calc.strike++;//Judges Strike it is.
 				} else bbr_calc.ball++;
 			}
@@ -168,6 +168,6 @@ BBR CheckBBR(BB *bb_srv, *BB bb_cli) {
 void Error(char *message)
 {
 	fputs(message, stderr);
-	fputc("\n", stderr);
+	fputc('\n', stderr);
 	exit(1);
 }
