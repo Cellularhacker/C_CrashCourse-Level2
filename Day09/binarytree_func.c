48be1409ea2c66141a2d907ff053a3a20b5cfe96
Cellularhacker <wva3cdae@gmail.com>
2016-08-11 14:37:48 +0900

    Removed Useless Funtion

--- Day09/binarytree_func.c
+++ Day09/binarytree_func.c
@@ -89,62 +89,6 @@ Node* removeNode(Node* Tree, int data) {
     return Tree;
 }
 
-/*
-//Destroy the Node
-void DestroyNode(Node *root, int data) {
-    Node *parent, *child, *successor, *successor_p, *target;
-    
-    parent = NULL; //Parent Node's Pointer
-    target = root; //Child Node's Pointer with current Parent Node. 
-    
-    //Searching Node which has that you want to destroy from the tree.
-    while(target != NULL && target->data != data) {
-        parent = target;
-        target = (data < target->data) ? target->left : target->right;
-    }
-    
-    if(target == NULL){ //The data what you want destroy doesn't exist.
-        printf("해당 값을 찾을 수 없습니다.\n");
-        return;
-    }
-    //In first case: Current node is terminal.
-    if(target->left == NULL && target->right == NULL) {
-        if(parent != NULL) {
-            if(parent->left == target) parent->left = NULL; //In case of left is the one.
-            else parent->right = NULL; //In case of right is the one.
-        } else root = NULL; //ifParent node is null, the one is the root one.
-    }
-
-    //In second case: Parent node has only one child node.
-    else if((target->left==NULL) || (target->right == NULL)) {
-        child = (target->left!=NULL) ? target->left : target->right;
-        if(parent != NULL){
-            if(parent->left == target) parent->left=child; //Connets Parent node with Child Node
-            else parent->right = child;
-        } else //ifParent node is null, the one is the root one.
-            root = child;
-    }
-    //In third case: Parent node has two child node.
-    else {
-        //Finds successor in subtree from right.
-        successor_p = target;
-        successor = target->right;
-        //Keep moves left to find successor.
-        while(successor->left != NULL) {
-            successor_p = successor;
-            successor = successor->left;
-        }
-        //Connnects successor, Parent and Child.
-        if(successor_p->left == successor) successor_p->left = successor->right;
-        else successor_p->right = successor->right;
-        //후속자가 가진 키값을 현재 노드에 복사
-        target->data = successor->data;
-        //원래의 후속자 삭제
-        target = successor;
-    }
-
-    free(target);
-}*/
 
 //Destroy the TREE
 void DestroyTree(Node* root) {
